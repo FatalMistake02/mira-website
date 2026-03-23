d9b2e4ca59393137c8b222af694d5dbe0d41b777
Kolbe Tessarzik <kolbe.tessarzik@gmail.com>
2026-03-23 18:46:25 -0400

    add update revalidation time again, but shorter

--- app/downloads/page.tsx
+++ app/downloads/page.tsx
@@ -317,7 +317,7 @@ function buildDownloadSlots(release: GitHubRelease): {
 
 async function fetchReleases(): Promise<GitHubRelease[]> {
   const response = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases`, {
-    cache: "no-cache",
+    next: { revalidate: 60 },
     headers: {
       Accept: "application/vnd.github+json",
       "User-Agent": "mira-website",
